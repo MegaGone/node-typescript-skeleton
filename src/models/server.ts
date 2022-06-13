0e4edcf6e3ef58a6dddff5dc92695fbc757955c1
Javier Martínez <jmartinez@develsystems.com>
2022-06-12 20:46:00 -0600

    Middlewares added and public directory added

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 class Server {
 
@@ -8,8 +9,19 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8080';
+
+        this.middlewares();
     }
 
+    private middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+    }
+
+    /**
+     * RESTSERVER LISTEN ON LOCALHOST
+     */
     public listen() {
         this.app.listen(this.port, () => {
             console.log(`Server run on http://localhost:${this.port}/`);
